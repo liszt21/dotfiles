b74f494b6b4719ac9db5668799325e742edb0178
liszt21 <liszt21@qq.com>
2023-07-09 22:47:25 +0800

    feat(home): add graph packages

--- home.scm
+++ home.scm
@@ -33,11 +33,71 @@
 (define machine-name (vector-ref (uname) 1))
 (define minisforum? (if (eq? machine-name "minisforum") #t #f))
 
-(define linux-packages (list "zoxide" "direnv"))
-(define emacs-packages (list "emacs-vterm" "emacs-rime" "tree-sitter-python" "tree-sitter-julia" "tree-sitter-typescript" "tree-sitter-javascript" "tree-sitter-c" "tree-sitter-cpp" "mu" "offlineimap3" "isync" "msmtp" "libvterm"))
-(define common-packages (list "glibc-locales" "git" "unzip" "zile" "recutils" "findutils" "syncthing" "fish" "bat" "neovim" "docker" "librime")) 
-(define font-packages (list "font-sarasa-gothic" "font-adobe-source-code-pro" "font-fira-code" "font-lxgw-wenkai" "font-gnu-unifont" "emacs-all-the-icons"))
-(define minisforum-package (list "nyxt" "qemu" "virt-manager" "virt-viewer" "vlc" "mpv" "gimp" "blender" "python-pywal" "mpv" "yt-dlp" "ffmpeg" "tesseract-ocr" "tesseract-ocr-tessdata-fast" "xclip"))
+(define font-packages 
+  (list 
+    "font-sarasa-gothic" 
+    "font-adobe-source-code-pro" 
+    "font-fira-code" 
+    "font-lxgw-wenkai" 
+    "font-gnu-unifont" 
+    "emacs-all-the-icons"))
+(define linux-packages 
+  (list 
+    "zoxide" 
+    "direnv"))
+(define emacs-packages 
+  (list 
+    "mu" 
+    "msmtp"
+    "isync" 
+    "libtool"
+    "librime"
+    "libvterm"
+    "offlineimap3"
+    "tree-sitter-c"
+    "tree-sitter-cpp"
+    "tree-sitter-julia"
+    "tree-sitter-python"
+    "tree-sitter-typescript"
+    "tree-sitter-javascript"))
+(define common-packages 
+  (list 
+    "glibc-locales"
+    "bat"
+    "git"
+    "zile"
+    "unzip"
+    "neovim"
+    "docker" 
+    "recutils"
+    "findutils"
+    "python-pywal"
+    "syncthing")) 
+(define minisforum-packages
+  (list 
+    "vlc"
+    "mpv"
+    "xclip"
+    "nyxt"
+    "gimp"
+    "qemu"
+    "yt-dlp"
+    "ffmpeg"
+    "blender"
+    "virt-manager"
+    "virt-viewer"
+    "tesseract-ocr"
+    "tesseract-ocr-tessdata-fast"))
+
+(define graph-packages
+  (list
+    "sddm"
+    "rofi"
+    "picom"
+    "xmonad"
+    "stumpwm"
+    "xorg-server"
+    "ghc-xmonad-contrib"))
 
 (define common-services 
   (list
@@ -56,5 +116,6 @@
                  font-packages
                  emacs-packages
                  linux-packages
-                 (if minisforum? minisforum-package (list)))))
+                 graph-packages
+                 (if minisforum? minisforum-packages (list)))))
   (services (append common-services)))
